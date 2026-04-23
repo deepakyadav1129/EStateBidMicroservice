6575dad014823fc3348be855c2bdf82bb9cbf65d
deepakyadav1129 <deepakyadav@1129@gmail.com>
2026-04-23 17:31:02 +0530

    Added Migration for biddingService

---
+++ Src/BiddingService/Migrations/20260423115943_Initial-creation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BiddingService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialcreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Auctions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AuctionEnd = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Seller = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReservePrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsFinished = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Auctions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bids",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AuctionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Bidder = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BidTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    BidStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bids", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Auctions");
+
+            migrationBuilder.DropTable(
+                name: "Bids");
+        }
+    }
+}
